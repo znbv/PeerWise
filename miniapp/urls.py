ec01eba21e70814bff0083f2ca4b8d15ae1b568a
Zainab Mohamed <mszainabsm@gmail.com>
2025-07-15 15:15:45 +0300

    ----------------

--- miniapp/urls.py
+++ miniapp/urls.py
@@ -1,19 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path(
-        "", views.TutorListView.as_view(), name="tutors"
-    ),  # tutors is the name of the URL pattern, which can be used in templates to link to this view
-    path(
-        "request/<int:pk>/",
-        views.StudentRequestCreateView.as_view(),
-        name="student_request",
-    ),
-    path(
-        "feedback/<int:pk>/", views.FeedbackCreateView.as_view(), name="leave_feedback"
-    ),
+    path( "", views.TutorListView.as_view(), name="tutors"),  # tutors is the name of the URL pattern, which can be used in templates to link to this view
+    path("request/<int:pk>/", views.StudentRequestCreateView.as_view(),name="student_request"),
+    path("feedback/<int:pk>/", views.FeedbackCreateView.as_view(), name="leave_feedback"),
     path("thankyou/", views.thankyou, name="thankyou"),  # URL for the thank you page
     path("adminaccess/", views.TutorViewForAdmin.as_view(), name="tutorlistadmin"),
     path("adminaccess/<int:pk>/update", views.update_tutor, name="update_tutor"),
-   # path("adminaccess/<int:pk>/delete", views.delete_tutor, name="delete_tutor"),
+    #path("adminaccess/<int:pk>/delete", views.delete_tutor, name="delete_tutor"),
 ]
